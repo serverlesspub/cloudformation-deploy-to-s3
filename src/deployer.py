58f198d156c564eb476f0e3429e5e50c2150823e
Aleksandar Simovic <alexander.simovic@gmail.com>
2019-01-02 01:28:53 +0100

    minor refactor

--- src/deployer.py
+++ src/deployer.py
@@ -3,25 +3,23 @@ import boto3
 import mimetypes
 s3 = boto3.resource('s3')
 
-def hello(event, context):
+def handler(event, context):
   target_bucket = os.environ['TARGET_BUCKET']
   lambda_src = os.getcwd()
   acl = 'private'
   cacheControl = 'max-age=600'
   
+  upload(lambda_src, target_bucket, acl, cacheControl)
+  return event
+
+def upload(lambda_src, target_bucket, acl, cacheControl):
   for folder, subs, files in os.walk(lambda_src):
     for filename in files:
         source_file_path = os.path.join(folder, filename)
         destination_s3_key = os.path.relpath(source_file_path, lambda_src)
         contentType, encoding = mimetypes.guess_type(source_file_path)
-        upload(source_file_path, target_bucket, destination_s3_key, s3, acl, cacheControl, contentType)
+        upload_file(source_file_path, target_bucket, destination_s3_key, s3, acl, cacheControl, contentType)
 
-  message = 'Hello {}!'.format(event)
-  return {
-      'message': message
-  }
-
-
-def upload(source, bucket, key, s3lib, acl, cacheControl, contentType):
-  print('uploading from {} {} {}'.format(source, bucket, key))
-  s3lib.Object(bucket, key).put(ACL=acl,Body=open(source, 'rb'),CacheControl=cacheControl,ContentType=contentType)
+def upload_file(source, bucket, key, s3lib, acl, cacheControl, contentType):
+    print('uploading from {} {} {}'.format(source, bucket, key))
+    s3lib.Object(bucket, key).put(ACL=acl,Body=open(source, 'rb'),CacheControl=cacheControl,ContentType=contentType)
