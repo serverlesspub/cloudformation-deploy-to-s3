84f0c6c68546a930c4e836bc10657b498b94c2f7
Aleksandar Simovic <alexander.simovic@gmail.com>
2019-01-10 22:56:15 +0100

    minor fix for physical resource id

--- src/deployer.py
+++ src/deployer.py
@@ -12,18 +12,20 @@ def resource_handler(event, context):
     target_bucket = event['ResourceProperties']['TargetBucket']
     lambda_src = os.getcwd()
     acl = event['ResourceProperties']['Acl']
-    cacheControl = 'max-age=' + event['ResourceProperties']['CacheControlMaxAge']
+    cacheControl = 'max-age=' + \
+        event['ResourceProperties']['CacheControlMaxAge']
     print(event['RequestType'])
     if event['RequestType'] == 'Create' or event['RequestType'] == 'Update':
       print('uploading')
       upload(lambda_src, target_bucket, acl, cacheControl)
     else:
       print('ignoring')
-    
+
     send_result(event)
   except Exception as err:
     send_error(event, err)
   return event
+
 
 def upload(lambda_src, target_bucket, acl, cacheControl):
   for folder, subs, files in os.walk(lambda_src):
@@ -31,31 +33,42 @@ def upload(lambda_src, target_bucket, acl, cacheControl):
         source_file_path = os.path.join(folder, filename)
         destination_s3_key = os.path.relpath(source_file_path, lambda_src)
         contentType, encoding = mimetypes.guess_type(source_file_path)
-        upload_file(source_file_path, target_bucket, destination_s3_key, s3, acl, cacheControl, contentType)
+        upload_file(source_file_path, target_bucket,
+                    destination_s3_key, s3, acl, cacheControl, contentType)
+
 
 def upload_file(source, bucket, key, s3lib, acl, cacheControl, contentType):
     print('uploading from {} {} {}'.format(source, bucket, key))
-    s3lib.Object(bucket, key).put(ACL=acl,Body=open(source, 'rb'),CacheControl=cacheControl,ContentType=contentType)
+    s3lib.Object(bucket, key).put(ACL=acl, Body=open(source, 'rb'),
+                                  CacheControl=cacheControl, ContentType=contentType)
+
 
 def send_result(event):
   response_body = json.dumps({
     'Status': 'SUCCESS',
-		'PhysicalResourceId': 'zeka123',
-		'StackId': event['StackId'],
-		'RequestId': event['RequestId'],
-		'LogicalResourceId': event['LogicalResourceId']
+    'PhysicalResourceId': get_physical_resource_id(event),
+    'StackId': event['StackId'],
+    'RequestId': event['RequestId'],
+    'LogicalResourceId': event['LogicalResourceId']
   })
   print(response_body)
-  requests.put(event['ResponseURL'], data = response_body)
+  requests.put(event['ResponseURL'], data=response_body)
+
 
 def send_error(event, error):
   response_body = json.dumps({
     'Status': 'FAILED',
     'Reason': str(error),
-		'PhysicalResourceId': event['PhysicalResourceId'] or event['RequestId'],
-		'StackId': event['StackId'],
-		'RequestId': event['RequestId'],
-		'LogicalResourceId': event['LogicalResourceId']
+    'PhysicalResourceId': get_physical_resource_id(event),
+    'StackId': event['StackId'],
+    'RequestId': event['RequestId'],
+    'LogicalResourceId': event['LogicalResourceId']
   })
   print(response_body)
-  requests.put(event['ResponseURL'], data = response_body)
+  requests.put(event['ResponseURL'], data=response_body)
+
+def get_physical_resource_id(event):
+  if 'PhysicalResourceId' in event.keys():
+    return event['PhysicalResourceId']
+  else:
+    return event['RequestId']
